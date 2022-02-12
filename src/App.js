1c7fffd8613397af0c34d0c6c2525d6b4cc7bb24
tobeken <tobik0124@gmail.com>
2022-02-12 22:42:15 +0900

    implement usign multiple states

--- src/App.js
+++ src/App.js
@@ -1,58 +1,29 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment2 = () => {
-    setCount((previousCount) => previousCount + 1);
-  };
-  const decrement2 = () => {
-    setCount((previousCount) => previousCount - 1);
-  };
   const reset = () => {
-    setCount(0);
-  };
-  const double = () => {
-    setCount((previousCount) => previousCount * 2);
-  };
-
-  // const devide = () => {
-  //   if (count % 3 === 0) {
-  //     setCount(count / 3);
-  //   } else {
-  //     return count;
-  //   }
-  // };
-
-  //
-
-  const devide = () => {
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
+    setPrice(props.price);
+    setName(props.name);
   };
   return (
     <>
-      <div>count:{count}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>+1</button>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
+      <p>
+        現在の{name}は{price}です
+        <button onClick={() => setPrice(price + 1)}>+1</button>
+        <button onClick={() => setPrice(price - 1)}>-1</button>
         <button onClick={reset}>Reset</button>
-        <button onClick={double}>*2</button>
-        <button onClick={devide}>割る３のあまり</button>
-      </div>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </p>
     </>
   );
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
 };
 
 export default App;
