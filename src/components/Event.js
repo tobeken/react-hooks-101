732debec215b3f9af98fa3869a78b71e2c94056e
tobeken <tobik0124@gmail.com>
2022-02-13 23:25:49 +0900

    delete operationlog

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { DELETE_EVENT } from "../actions";
+import { DELETE_EVENT, ADD_OPERATION_LOG } from "../actions";
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils";
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext);
@@ -14,6 +15,11 @@ const Event = ({ event }) => {
       dispatch({
         type: DELETE_EVENT,
         id,
+      });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `イベント(id=${id})を削除しました`,
+        operatedAt: timeCurrentIso8601(),
       });
     }
   };
