e3244fe5522f425630f2c88a6cc5a3a50f1297e7
tobeken <tobik0124@gmail.com>
2022-02-13 23:13:19 +0900

    refactor iso

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext } from "react";
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions";
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_OPERATION_LOGS,
+} from "../actions";
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils";
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -14,6 +20,11 @@ const EventForm = () => {
       title,
       body,
     });
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: "イベントを作成しました",
+      operatedAt: timeCurrentIso8601(),
+    });
     setTitle("");
     setBody("");
   };
@@ -25,6 +36,11 @@ const EventForm = () => {
     );
     if (result) {
       dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: "すべてのイベントを削除しました",
+        operatedAt: timeCurrentIso8601(),
+      });
     }
   };
 
